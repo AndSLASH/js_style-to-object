3fe712cfe5af73acc80241fc9f484571d3d664d3
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-05-02 16:21:38 +0300

    my solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+  const totalStylesArray = sourceString.split(';');
+  const clearedStylesArray = totalStylesArray
+    .map((style) => style.trim())
+    .filter((style) => style !== '');
+
+  clearedStylesArray.forEach((style) => {
+    const [cssName, cssValue] = style.split(':');
+
+    if (cssName && cssValue) {
+      const trimmedСssName = cssName.trim();
+      const trimmedCssValue = cssValue.trim();
+
+      stylesObject[trimmedСssName] = trimmedCssValue;
+    }
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
